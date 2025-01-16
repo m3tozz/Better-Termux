c7a4fc9a31ea93713b54cb475599fbfd84ff1c08
Metin Zühre <79897762+m3tozz@users.noreply.github.com>
2025-01-16 14:00:45 +0300

    Update better-termux.sh

--- better-termux.sh
+++ better-termux.sh
@@ -66,7 +66,7 @@ cp -r .zshrc /data/data/com.termux/files/home/
 echo -e "\033[1;31m Installıng NeoCat\033[01;32m"
 pkg install neofetch -y
 neofetch
-git clone https://github.com/m3tozz/NeoCat-Termux.git
+git clone https://github.com/m3tozz/NeoCat.git
 rm -r /data/data/com.termux/files/home/.config/neofetch/config.conf
 cd NeoCat/Termux-Themes/Palm/neofetch
 cp -r config.conf /data/data/com.termux/files/home/.config/neofetch/
