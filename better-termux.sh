2b647900bb1ccc54a17dfc9f759569f9f6439852
Metin Zühre <79897762+m3tozz@users.noreply.github.com>
2025-07-09 00:36:44 +0300

    Add files via upload

---
+++ better-termux.sh
@@ -0,0 +1,128 @@
+########################################
+# Made By M3TOZZ                       #
+# https://m3tozz.github.io             #
+########################################
+
+# BetterTermux Version
+    version='BetterTermux-1.0'
+
+#Colors
+    red='\e[1;31m'
+    yellow='\e[1;33m'
+    blue='\e[1;34m'
+    tp='\e[0m'
+    green='\e[0;32m'
+    bgreen='\033[1;32m'
+
+# Define Constants.
+# Define Constants.
+export APP="BetterTermux" 		# Project Name
+export CWD="${PWD}"			# Current Work Directory
+export BASENAME="${0##*/}"	# Base Name of This Script
+
+# Functions.
+help() {
+	echo -e "Wrong usage, there is 3 arguments for ${BASENAME}\n
+\t${BASENAME} --install: it's install's the ${APP} project on your Termux.
+\t${BASENAME} --uninstall: it's uninstall's the ${APP} project on your Termux.
+\t${BASENAME} --version: show the version.
+\t${BASENAME} --help: show this page.
+"
+
+}
+
+bt:version() {
+echo "$version"
+}
+
+bt:install() {
+echo -e "\033[1;31m 
+   ___      __  __            
+  / _ )___ / /_/ /____ ____   
+ / _  / -_) __/ __/ -_) __/   
+/____/\__/\__/\__/\__/_/      
+/_  __/__ ______ _  __ ____ __
+ / / / -_) __/  ' \/ // /\ \ /
+/_/  \__/_/ /_/_/_/\_,_//_\_\ 
+Installing...                          
+\033[0m"
+sleep 3
+clear
+echo -e "\033[1;31m Updating PKG Repo\033[01;32m"
+mv zshrc .zshrc
+pkg update -y
+echo -e "\033[1;31m Installıng Zsh\033[01;32m"
+pkg install zsh -y
+chsh -s zsh 
+mkdir -p /data/data/com.termux/files/home/.zsh/plugins
+mkdir -p /data/data/com.termux/files/home/.zsh/themes
+git clone https://github.com/zdharma-zmirror/fast-syntax-highlighting.git
+cp -r  fast-syntax-highlighting /data/data/com.termux/files/home/.zsh/plugins
+git clone https://github.com/zsh-users/zsh-autosuggestions.git
+cp -r  zsh-autosuggestions /data/data/com.termux/files/home/.zsh/plugins
+git clone https://github.com/zsh-users/zsh-completions.git
+cp -r  /zsh-completions /data/data/com.termux/files/home/.zsh/plugins
+cp -r .zshrc /data/data/com.termux/files/home/
+echo -e "\033[1;31m Installıng NeoCat\033[01;32m"
+pkg install neofetch -y
+neofetch
+git clone https://github.com/m3tozz/NeoCat.git
+rm -r /data/data/com.termux/files/home/.config/neofetch/config.conf
+cd NeoCat/Termux-Themes/Palm/neofetch
+cp -r config.conf /data/data/com.termux/files/home/.config/neofetch/
+cp -r ascii.txt /data/data/com.termux/files/home/.config/neofetch/
+cd
+rm /data/data/com.termux/files/usr/etc/motd
+clear
+zsh
+}
+
+bt:uninstall() {
+echo -e "\033[1;31m 
+   ___      __  __            
+  / _ )___ / /_/ /____ ____   
+ / _  / -_) __/ __/ -_) __/   
+/____/\__/\__/\__/\__/_/      
+/_  __/__ ______ _  __ ____ __
+ / / / -_) __/  ' \/ // /\ \ /
+/_/  \__/_/ /_/_/_/\_,_//_\_\ 
+Uninstalling...                          
+\033[0m"
+sleep 3
+clear
+echo -e "\033[1;31m Unnstallıng NeoCat-Termux\033[01;32m"
+rm -r /data/data/com.termux/files/home/NeoCat-Termux
+rm -r /data/data/com.termux/files/home/.config/neofetch
+echo -e "\033[1;31m Uninstallıng Zsh\033[01;32m"
+chsh -s bash 
+pkg remove zsh -y
+clear
+echo -e "\033[1;31m Better-Termux has been removed your shell\033[0m"
+bash
+}
+
+help() {
+	echo -e "	 
+--install: it's install's the ${APP} project on your distrubution.
+--uninstall: it's uninstall's the ${APP} project on your distrubution.
+--version: show the version.
+--help: show this page."
+}
+
+
+
+# Argument Parser.
+case "${1,,}" in
+	"--install"|"-i")
+		bt:install
+	;;
+	"--uninstall"|"-u")
+		bt:uninstall
+	;;
+        	"--version"|"-v")
+		bt:version
+	;;
+	*)
+		help
+	;;
+esac
---
+++ zshrc
@@ -0,0 +1,77 @@
+# Set up the prompt
+
+autoload -Uz promptinit
+promptinit
+prompt adam1
+
+setopt histignorealldups sharehistory
+
+# Use emacs keybindings even if our EDITOR is set to vi
+bindkey -e
+
+# Keep 1000 lines of history within the shell and save it to ~/.zsh_history:
+HISTSIZE=1000
+SAVEHIST=1000
+HISTFILE=~/.zsh_history
+
+# Use modern completion system
+autoload -Uz compinit
+compinit
+
+zstyle ':completion:*' auto-description 'specify: %d'
+zstyle ':completion:*' completer _expand _complete _correct _approximate
+zstyle ':completion:*' format 'Completing %d'
+zstyle ':completion:*' group-name ''
+zstyle ':completion:*' menu select=2
+eval "$(dircolors -b)"
+zstyle ':completion:*:default' list-colors ${(s.:.)LS_COLORS}
+zstyle ':completion:*' list-colors ''
+zstyle ':completion:*' list-prompt %SAt %p: Hit TAB for more, or the character to insert%s
+zstyle ':completion:*' matcher-list '' 'm:{a-z}={A-Z}' 'm:{a-zA-Z}={A-Za-z}' 'r:|[._-]=* r:|=* l:|=*'
+zstyle ':completion:*' menu select=long
+zstyle ':completion:*' select-prompt %SScrolling active: current selection at %p%s
+zstyle ':completion:*' use-compctl false
+zstyle ':completion:*' verbose true
+
+zstyle ':completion:*:*:kill:*:processes' list-colors '=(#b) #([0-9]#)*=0=01;31'
+zstyle ':completion:*:kill:*' command 'ps -u $USER -o pid,%cpu,tty,cputime,cmd'
+source /data/data/com.termux/files/home/.zsh/plugins/fast-syntax-highlighting/fast-syntax-highlighting.plugin.zsh
+source /data/data/com.termux/files/home/.zsh/plugins/zsh-autosuggestions/zsh-autosuggestions.zsh
+fpath=(/data/data/com.termux/files/home/.zsh/plugins/zsh-completions/src $fpath)
+
+# cd && ls
+cd() {
+  if [ $# -eq 0 ]; then
+    builtin cd ~ || return
+  else
+    builtin cd "$@" || return
+  fi
+  ls
+}
+
+# BetterTermux Updater
+bettertermux_updater() {
+  bettertermux_path="$HOME/.bettertermux/BetterTermux/better-termux.sh"
+
+  if [ -f "$bettertermux_path" ]; then
+    remote_ver=$(curl -s https://raw.githubusercontent.com/m3tozz/BetterTermux/main/better-termux.sh | grep -E "^ *version=" | cut -d"'" -f2)
+    local_ver=$(grep -E "^ *version=" "$bettertermux_path" | cut -d"'" -f2)
+
+    if [ "$remote_ver" != "$local_ver" ]; then
+      echo -e "\e[1;32mBetterTermux update found: $local_ver → $remote_ver\e[0m"
+      echo "Updating..."
+      bash "$bettertermux_path" --install
+      cd
+      export BETTERTERMUX_UPDATED=1
+      return 0
+    fi
+  fi
+  return 1
+}
+
+if [ -z "$BETTERTERMUX_UPDATED" ]; then
+  bettertermux_updater
+fi
+
+bettertermux_updater
+neofetch
